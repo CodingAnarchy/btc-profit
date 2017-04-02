74aa84f4c58b86ab93a20f9c194a5c39e09f8969
Matt Tanous <mtanous22@gmail.com>
2017-04-02 17:29:37 -0600

    Adjust for sales

--- coinbase.rb
+++ coinbase.rb
@@ -5,13 +5,19 @@ require 'coinbase/wallet'
 I18n.load_path = ['en.yml']
 client = Coinbase::Wallet::Client.new(api_key: ENV['COINBASE_API_KEY'], api_secret: ENV['COINBASE_API_SECRET'])
 
-usd_total = BigDecimal.new(0)
+usd_buy_total = BigDecimal.new(0)
 btc_total = BigDecimal.new(0)
 client.list_buys(client.primary_account.id, fetch_all: true).each do |data, resp|
-  usd_total += BigDecimal.new(data['total']['amount'])
+  usd_buy_total += BigDecimal.new(data['total']['amount'])  # Add money spent on buying bitcoin
   btc_total += BigDecimal.new(data['amount']['amount'])
 end
 
+usd_sell_total = BigDecimal.new(0)
+client.list_sells(client.primary_account.id, fetch_all: true).each do |data, resp|
+  usd_sell_total += BigDecimal.new(data['total']['amount']) # Realized returns from sales
+  btc_total -= BigDecimal.new(data['amount']['amount'])  # Subtract sold bitcoin from current holdings
+end
+
 current_value = btc_total * BigDecimal.new(client.spot_price['amount'])
-profit = current_value - usd_total
+profit = current_value + usd_sell_total - usd_buy_total
 puts "Profit: #{profit.to_money.format}"
